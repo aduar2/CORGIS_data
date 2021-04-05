0e41bec2d401832bad5b41fe1cc7b67f08253c8e
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-04-05 09:54:49 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route("/tallestBuilt")
 def home():
-    return render_template('tallestBuilt.html', options = get_)
+    return render_template('tallestBuilt.html', options = get_year_options())
 
 @app.route("/tallestBuilt")
 def response():
     year = request.args['year']
-    return render_template('tallestBuiltResponse.html', , )
+    return render_template('tallestBuiltResponse.html', options = get_year_options(), response = get_tallest(year))
     
 def find_years_range():
     firstYear = 2021
@@ -32,9 +32,9 @@ def find_years_range():
             if backUp > 0
             {
                 if backUp + average < firstYear:
-                    firstYear = backUp
+                    firstYear = backUp + average
                 if backUp + average > lastyear:
-                    lastYear = backUp
+                    lastYear = backUp + average
             }
         }
         return "contains data on buildings built from " + firstYear + " to " + lastYear
@@ -45,20 +45,40 @@ def find_average_completion():
         skyscrapers = json.load(skyscraper_data)
     completed = 0
     started = 0
-    count = 0
+    average = 0
     for skyscraper in skyscrapers:
         completed = skyscraper["status"]["completed"]["year"]
         started = skyscraper["started"]["year"]
         if not started == 0 or not completed == 0:
             time = completed - started
-        completionTime[count] = time
-        count = count + 1
+        completionTime.append(time)
     for x in completionTime:
-        return x
+        average = average + x
+        x = x + 1
+    average = average / len(completionTime)
+    return average
     
 def get_year_options():
     years = []
-    year = skyscraper["status"]["completed"]["year"]
     with open('skyscrapers.json') as skyscraper_data:
         skyscrapers = json.load(skyscraper_data)
-    skyscraper
+    for skyscraper in skyscrapers:
+        year = skyscraper["status"]["completed"]["year"]
+        if year > 0
+            years.append(year)
+    years.sort()
+    return years
+
+def get_tallest(year):
+    with open('skyscrapers.json') as skyscraper_data
+    skyscrapers = json.load(skyscraper_data)
+    allBuilt = []
+    allBuiltHeights = []
+    for skyscraper in skyscrapers:
+        if skyscraper["status"]["completed"]["year"] == year:
+            allBuilt.append(skyscraper["name"])
+            allBuiltHeights.append("skyscraper["statistics"]["height"])
+    tallest = "none"
+    tallestHeight = 0
+    for x in allBuiltHeights:
+        if x > talleatHeight:
