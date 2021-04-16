21bbca41ac5e762a7372d7a460c8a2344fb4b733
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-04-16 09:19:32 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -32,6 +32,9 @@ def graphResponse():
         m = percents[3]
         sc = percents[4]
         return render_template('pieChart.html', concrete = con, steel = s, composite = com, masonry = m, steelConcrete = sc)
+    if graph == "lineGraph":
+        return render_template('lineGraph.html', points = get_line_data)
+
 
 def find_average_completion():
     completionTime = []
